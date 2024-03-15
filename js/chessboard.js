6b6309ec5829657baa056c2c71cb5f40d1055bb9
scottdeutsch40 <scottdeutsch40@gmail.com>
2024-03-15 16:34:29 -0400

    Board appended

--- js/chessboard.js
+++ js/chessboard.js
@@ -1,20 +1,27 @@
 export default {
-	draw,
-	highlight
+  draw,
+  highlight,
 };
 
-
+var origBoardEl;
 // ****************************
 
 function draw(boardEl) {
-	// TODO: draw the chessboard, 8 rows (divs)
-	// of 8 tiles (divs) each, inserting all DOM
-	// elements into `boardEl` div
+  origBoardEl = boardEl;
+
+  for (let i = 0; i < 8; i++) {
+    let rowEl = document.createElement("div");
+    for (let j = 0; j < 8; j++) {
+      let tileEl = document.createElement("div");
+      rowEl.appendChild(tileEl);
+    }
+  }
+  boardEl.appendChild(rowEl);
 }
 
 function highlight(tileEl) {
-	// TODO: clear previous highlights (if any) and
-	// then find the tiles in the two diagonals
-	// (major and minor) that `tileEl` belongs to,
-	// to highlight them via CSS class "highlighted"
+  // TODO: clear previous highlights (if any) and
+  // then find the tiles in the two diagonals
+  // (major and minor) that `tileEl` belongs to,
+  // to highlight them via CSS class "highlighted"
 }
